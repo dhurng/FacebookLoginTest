8cc93bd9f4acca6427506ec3ddf94ca36955fb25
david hurng <dhurng@gmail.com>
2015-09-02 17:33:53 -0700

    Facebook Login for Swift
    Test Facebook Login using Swift

---
+++ FacebookTest/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  FacebookTest
+//
+//  Created by David Hurng on 9/2/15.
+//  Copyright (c) 2015 Pupil. All rights reserved.
+//
+
+#ifndef FacebookTest_Bridging_Header_h
+#define FacebookTest_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
